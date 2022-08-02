2f69ea13aa1cd6f0b3259691feea354648c94b57
mingzyy <mingzyy@naver.com>
2022-08-02 22:47:08 +0900

    주석추가 테스트

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>*/
-
+    //주석추가!!
     <div>
       <Counter />
       <div>counter : {counter}</div>
